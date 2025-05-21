ff442174fbc43bab5e66449fa090f1caa75a7342
vinc3138 <vincent.m.liogier@gmail.com>
2025-05-21 14:30:35 +0200

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-
 const express = require('express');
-const connectDB = require('./db/connectDB');
+
+//const express = require('express');
+//const connectDB = require('./db/connectDB');
 
 const app = express();
 const port = process.env.PORT || 4002;
@@ -9,11 +10,11 @@ const port = process.env.PORT || 4002;
 app.use(express.json());
 
 // Connecte MongoDB
-connectDB();
+//connectDB();
 
 // Routes
-const userRoutes = require('../routes/users');
-app.use('/api/users', userRoutes);
+//const userRoutes = require('../routes/users');
+//app.use('/api/users', userRoutes);
 
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
